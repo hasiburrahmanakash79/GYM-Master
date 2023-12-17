c98cf4c8a552f653015133b53c15587a3c4507c6
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-12-17 21:04:17 +0600

    make repo for gym master and first commit

--- src/Pages/Shared/Nav/Navbar.jsx
+++ src/Pages/Shared/Nav/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
         <Link to="/">Contact us</Link>
       </li>
       <li>
-        <Link to="/">Login</Link>
+        <Link to="/" className="bg-yellow-500">Login</Link>
       </li>
     </>
   );
@@ -27,6 +27,7 @@ const Navbar = () => {
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              x
             </label>
             <ul
               tabIndex={0}
