01c47b2908453aae7db85a42032f79ed7d0f129f
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-12-19 13:40:50 +0600

    Feature bg and make card

--- src/Pages/HomePage/FeaturedClasses/FeaturedClasses.jsx
+++ src/Pages/HomePage/FeaturedClasses/FeaturedClasses.jsx
@@ -1,21 +1,114 @@
 const FeaturedClasses = () => {
   return (
-    <div className="bg-[url('https://static.vecteezy.com/system/resources/previews/022/653/988/original/treadmill-in-modern-gym-toned-image-3d-rendering-generative-ai-free-photo.jpg')] h-[100vh] bg-cover bg-fixed" >
-      
-    <div className="card w-96 bg-base-100 shadow-xl">
-      <div className="card-body text-center">
-        <h2 className="text-2xl font-bold">Cross fit</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="bg-[url('https://static.vecteezy.com/system/resources/previews/022/653/988/original/treadmill-in-modern-gym-toned-image-3d-rendering-generative-ai-free-photo.jpg')] bg-cover bg-fixed">
+      <div className=" bg-black flex items-center bg-opacity-70">
+        <div className="container mx-auto grid grid-cols-4 gap-6 py-20">
+          <div className="card  bg-base-100 shadow-xl">
+            <div className="card-body text-center">
+              <h2 className="text-2xl font-bold">Cross fit</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+
+            <figure>
+              <img
+                src="https://assets.gqindia.com/photos/5cee7eb00379a73d25177759/4:3/w_1440,h_1080,c_limit/Pushup.jpg"
+                alt="Shoes"
+              />
+            </figure>
+          </div>
+          <div className="card  bg-base-100 shadow-xl">
+            <div className="card-body text-center">
+              <h2 className="text-2xl font-bold">Cross fit</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+
+            <figure>
+              <img
+                src="https://assets.gqindia.com/photos/5cee7eb00379a73d25177759/4:3/w_1440,h_1080,c_limit/Pushup.jpg"
+                alt="Shoes"
+              />
+            </figure>
+          </div>
+          <div className="card   bg-base-100 shadow-xl">
+            <div className="card-body text-center">
+              <h2 className="text-2xl font-bold">Cross fit</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+
+            <figure>
+              <img
+                src="https://assets.gqindia.com/photos/5cee7eb00379a73d25177759/4:3/w_1440,h_1080,c_limit/Pushup.jpg"
+                alt="Shoes"
+              />
+            </figure>
+          </div>
+          <div className="card   bg-base-100 shadow-xl">
+            <div className="card-body text-center">
+              <h2 className="text-2xl font-bold">Cross fit</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+
+            <figure>
+              <img
+                src="https://assets.gqindia.com/photos/5cee7eb00379a73d25177759/4:3/w_1440,h_1080,c_limit/Pushup.jpg"
+                alt="Shoes"
+              />
+            </figure>
+          </div>
+          <div className="card   bg-base-100 shadow-xl">
+            <div className="card-body text-center">
+              <h2 className="text-2xl font-bold">Cross fit</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+
+            <figure>
+              <img
+                src="https://assets.gqindia.com/photos/5cee7eb00379a73d25177759/4:3/w_1440,h_1080,c_limit/Pushup.jpg"
+                alt="Shoes"
+              />
+            </figure>
+          </div>
+          <div className="card   bg-base-100 shadow-xl">
+            <div className="card-body text-center">
+              <h2 className="text-2xl font-bold">Cross fit</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+
+            <figure>
+              <img
+                src="https://assets.gqindia.com/photos/5cee7eb00379a73d25177759/4:3/w_1440,h_1080,c_limit/Pushup.jpg"
+                alt="Shoes"
+              />
+            </figure>
+          </div>
+          <div className="card   bg-base-100 shadow-xl">
+            <div className="card-body text-center">
+              <h2 className="text-2xl font-bold">Cross fit</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+
+            <figure>
+              <img
+                src="https://assets.gqindia.com/photos/5cee7eb00379a73d25177759/4:3/w_1440,h_1080,c_limit/Pushup.jpg"
+                alt="Shoes"
+              />
+            </figure>
+          </div>
+          <div className="card   bg-base-100 shadow-xl">
+            <div className="card-body text-center">
+              <h2 className="text-2xl font-bold">Cross fit</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+
+            <figure>
+              <img
+                src="https://assets.gqindia.com/photos/5cee7eb00379a73d25177759/4:3/w_1440,h_1080,c_limit/Pushup.jpg"
+                alt="Shoes"
+              />
+            </figure>
+          </div>
+        </div>
       </div>
-      
-      <figure>
-        <img
-          src="https://assets.gqindia.com/photos/5cee7eb00379a73d25177759/4:3/w_1440,h_1080,c_limit/Pushup.jpg"
-          alt="Shoes"
-        />
-        
-      </figure>
-    </div>
     </div>
   );
 };
