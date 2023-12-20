63659e41b2cb740c5448e2498c0d30a736b3162d
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-12-20 20:30:22 +0600

    set branch ansdhjbsh

--- src/Pages/HomePage/FeaturedClasses/FeaturedClasses.jsx
+++ src/Pages/HomePage/FeaturedClasses/FeaturedClasses.jsx
@@ -2,7 +2,13 @@ const FeaturedClasses = () => {
   return (
     <div className="bg-[url('https://static.vecteezy.com/system/resources/previews/022/653/988/original/treadmill-in-modern-gym-toned-image-3d-rendering-generative-ai-free-photo.jpg')] bg-cover bg-fixed">
       <div className=" bg-black flex items-center bg-opacity-70">
-        <div className="container mx-auto grid grid-cols-4 gap-6 py-20">
+        <div className="container mx-auto py-20">
+          <div className="pb-10 text-white text-center space-y-5">
+            <p className="text-red-500 text-xl ">GYM Master Classes</p>
+            <h1 className="text-4xl font-bold">Our Featured Classes</h1>
+            <p>Gymat an unknown printer took a galley of type and scrambled <br /> make a type specimen book.</p>
+          </div>
+          <div className=" grid grid-cols-4 gap-6 ">
           <div className="card  bg-base-100 shadow-xl">
             <div className="card-body text-center">
               <h2 className="text-2xl font-bold">Cross fit</h2>
@@ -108,6 +114,8 @@ const FeaturedClasses = () => {
             </figure>
           </div>
         </div>
+        </div>
+        
       </div>
     </div>
   );
