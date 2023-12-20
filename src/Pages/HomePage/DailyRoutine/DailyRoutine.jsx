1983e854209ce5f165c2d4fa62692c5565b24b84
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-12-20 20:57:42 +0600

    make DailyRoutine cardgh

--- src/Pages/HomePage/DailyRoutine/DailyRoutine.jsx
+++ src/Pages/HomePage/DailyRoutine/DailyRoutine.jsx
@@ -1,15 +1,45 @@
+import { FaChartLine, FaClipboardCheck, FaListAlt, FaSwimmer } from "react-icons/fa";
 
 const DailyRoutine = () => {
-    return (
-        <div>
-            <div className="card">
-                <h1>x</h1>
-                <h1>PROGRESSION</h1>
-                <p>Vestibulumipsum primis in faucibus orci luctus et ultrices posuere Curae onces</p>
-            </div>
-            
+  return (
+    <div className="container mx-auto py-20">
+      <div className="md:grid grid-cols-4 gap-8">
+        <div className="card p-5 items-center text-center space-y-3">
+          <FaChartLine  className="text-5xl text-red-500"/>
+          <h1 className="text-3xl font-bold">PROGRESSION</h1>
+          <p>
+            Vestibulumipsum primis in faucibus orci luctus et ultrices posuere
+            Curae onces
+          </p>
         </div>
-    );
+        <div className="card p-5 items-center text-center space-y-3">
+          <FaSwimmer  className="text-5xl text-red-500"/>
+          <h1 className="text-3xl font-bold">WORKOUT</h1>
+          <p>
+            Vestibulumipsum primis in faucibus orci luctus et ultrices posuere
+            Curae onces
+          </p>
+        </div>
+        <div className="card p-5 items-center text-center space-y-3">
+          <FaClipboardCheck  className="text-5xl text-red-500"/>
+          <h1 className="text-3xl font-bold">NUTRITION</h1>
+          <p>
+            Vestibulumipsum primis in faucibus orci luctus et ultrices posuere
+            Curae onces
+          </p>
+        </div>
+        <div className="card p-5 items-center text-center space-y-3">
+          <FaListAlt  className="text-5xl text-red-500"/>
+          
+          <h1 className="text-3xl font-bold">DAILY CHART</h1>
+          <p>
+            Vestibulumipsum primis in faucibus orci luctus et ultrices posuere
+            Curae onces
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DailyRoutine;
