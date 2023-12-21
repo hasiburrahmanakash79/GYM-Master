74c916ae10fb466b14c5d33911db090becedd4c4
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-12-22 00:11:57 +0600

    remove footer

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -2,6 +2,13 @@ const Footer = () => {
   return (
     <footer className="footer p-10 bg-base-200 text-base-content">
       <nav>
+        <header className="footer-title">Company</header>
+        <a className="link link-hover">About us</a>
+        <a className="link link-hover">Contact</a>
+        <a className="link link-hover">Jobs</a>
+        <a className="link link-hover">Press kit</a>
+      </nav>
+      <nav>
         <header className="footer-title">Services</header>
         <a className="link link-hover">Branding</a>
         <a className="link link-hover">Design</a>
@@ -14,14 +21,6 @@ const Footer = () => {
         <a className="link link-hover">Privacy policy</a>
         <a className="link link-hover">Cookie policy</a>
       </nav>
-      <nav>
-        <header className="footer-title">Company</header>
-        <a className="link link-hover">About us</a>
-        <a className="link link-hover">Contact</a>
-        <a className="link link-hover">Jobs</a>
-        <a className="link link-hover">Press kit</a>
-      </nav>
-      
       <form>
         <header className="footer-title">Newsletter</header>
         <fieldset className="form-control w-80">
