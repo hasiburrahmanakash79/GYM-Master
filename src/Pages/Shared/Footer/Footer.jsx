931e15498c56142c297e8132c982f6a0d3bb0866
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-12-21 16:31:35 +0600

    set branch

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -7,6 +7,7 @@ const Footer = () => {
         <a className="link link-hover">Design</a>
         <a className="link link-hover">Marketing</a>
         <a className="link link-hover">Advertisement</a>
+        <a className="link link-hover">Advertisement</a>
       </nav>
       <nav>
         <header className="footer-title">Company</header>
@@ -14,11 +15,13 @@ const Footer = () => {
         <a className="link link-hover">Contact</a>
         <a className="link link-hover">Jobs</a>
         <a className="link link-hover">Press kit</a>
+        <a className="link link-hover">Press kit</a>
       </nav>
       <nav>
         <header className="footer-title">Legal</header>
         <a className="link link-hover">Terms of use</a>
         <a className="link link-hover">Privacy policy</a>
+        <a className="link link-hover">Cookie policy</a>
         <a className="link link-hover">Cookie policy</a>
       </nav>
       <form>
