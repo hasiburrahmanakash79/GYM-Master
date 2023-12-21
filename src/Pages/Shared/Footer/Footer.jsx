7eb6428ee53a08d244c7972798eef2bfbc49fb3b
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-12-22 00:12:53 +0600

    remove footer text from

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -6,13 +6,11 @@ const Footer = () => {
         <a className="link link-hover">About us</a>
         <a className="link link-hover">Contact</a>
         <a className="link link-hover">Jobs</a>
-        <a className="link link-hover">Press kit</a>
       </nav>
       <nav>
         <header className="footer-title">Services</header>
         <a className="link link-hover">Branding</a>
         <a className="link link-hover">Design</a>
-        <a className="link link-hover">Marketing</a>
         <a className="link link-hover">Advertisement</a>
       </nav>
       <nav>
