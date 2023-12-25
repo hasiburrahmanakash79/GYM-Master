0573c7f8e111dbb235b8dbd636d881da698b9583
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-12-25 21:59:24 +0600

    remove lorem text

--- src/Pages/ClassPage/Classes.jsx
+++ src/Pages/ClassPage/Classes.jsx
@@ -3,7 +3,7 @@ const Classes = () => {
     return (
         <div>
             <h1>Classes</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, culpa quia saepe facere laborum, corporis quidem molestiae, minus adipisci porro qui assumenda numquam! Qui aut quas nihil ducimus, ea sunt mollitia reiciendis odio laudantium officia, corrupti animi ullam voluptate magni consequuntur error aliquam exercitationem quasi dicta ratione necessitatibus nostrum temporibus ut ab? Fuga eius tempore molestiae sunt reiciendis quisquam deleniti inventore quos sed corrupti error, corporis blanditiis repellat, rerum ex incidunt odit nisi in, id cumque. Quos quis fugit ipsum saepe, temporibus ipsam inventore fugiat odit maiores assumenda et rerum! Aperiam dolorum necessitatibus molestias magni culpa voluptatibus. Nihil, molestiae fuga!</p>
+            
         </div>
     );
 };
