6cae4837db6d22e53348e467a4b22923f8272440
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-12-26 23:44:09 +0600

    set branch

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -3,6 +3,7 @@ const Contact = () => {
     return (
         <div>
             <h1>Contact</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet neque blanditiis incidunt odio officia officiis, reiciendis molestias quis recusandae nulla adipisci voluptatibus nemo assumenda quos ullam quo nisi ea repellendus obcaecati atque libero esse temporibus numquam magnam? Cupiditate, incidunt odit omnis cumque quidem suscipit porro repellat iste possimus debitis distinctio maxime architecto quis? Veritatis id libero autem aperiam delectus! Autem, quaerat eius. Molestiae deleniti hic delectus, fugiat blanditiis iste inventore est, quia perspiciatis veniam atque odio voluptas soluta cum accusamus. Dolor architecto itaque eligendi quibusdam ab. Repellendus, corporis laudantium error eos nesciunt itaque rerum ipsam hic explicabo cum optio qui, nulla neque consequatur. Dolores ipsam enim molestias alias assumenda beatae ipsa tenetur libero, dignissimos in totam consequatur, illo quae excepturi obcaecati voluptatum commodi impedit voluptas esse. Dolorem veniam maiores nulla magni dolorum dolor tempora non, omnis nesciunt, deserunt molestiae autem fugit itaque minima possimus, ipsa recusandae animi sed. Error, nemo.</p>
         </div>
     );
 };
