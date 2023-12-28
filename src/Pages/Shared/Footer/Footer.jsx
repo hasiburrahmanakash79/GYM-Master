b81e8d4ee44788cb5fbb20e9582129ef47413487
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-12-28 23:53:06 +0600

    set kjjk

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -7,12 +7,7 @@ const Footer = () => {
         <a className="link link-hover">Contact</a>
         <a className="link link-hover">Jobs</a>
       </nav>
-      <nav>
-        <header className="footer-title">Services</header>
-        <a className="link link-hover">Branding</a>
-        <a className="link link-hover">Design</a>
-        <a className="link link-hover">Advertisement</a>
-      </nav>
+      
       <nav>
         <header className="footer-title">Legal</header>
         <a className="link link-hover">Terms of use</a>
