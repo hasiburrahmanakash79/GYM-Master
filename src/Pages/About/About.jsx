55c545e81c9fe05afae7823306be7e5e9ee360d0
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-12-30 15:18:48 +0600

    make a hook and change bg

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,7 +1,9 @@
+import SectionTitle from "../../Hooks/SectionTitle";
 
 const About = () => {
     return (
         <div>
+            <SectionTitle subTitle={"Hook"}></SectionTitle>
             <h1>About</h1>
         </div>
     );
