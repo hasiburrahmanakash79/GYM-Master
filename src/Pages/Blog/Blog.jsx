08fb1d189109ea1de69d44ac5213bcb0161cebc2
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-12-31 22:33:12 +0600

    make blog title

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -1,8 +1,10 @@
+import SectionTitle from "../../Hooks/SectionTitle";
 
 
 const Blog = () => {
     return (
         <div>
+           <SectionTitle subTitle={"Hook"} title={"Blogs"}></SectionTitle>
           <h1>Blog</h1>  
         </div>
     );
