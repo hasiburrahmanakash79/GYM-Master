a8643e1004e779cf31a704ae5c25f00069bce439
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-01-01 23:22:22 +0600

    make contact section

--- src/Hooks/SectionTitle.jsx
+++ src/Hooks/SectionTitle.jsx
@@ -1,6 +1,6 @@
 const SectionTitle = ({ subTitle, title }) => {
   return (
-    <div className="bg-[url('https://t4.ftcdn.net/jpg/03/50/81/89/360_F_350818931_54A6UVQiJIK8UHFWB0NTGSIKO9jyTbQP.jpg')] bg-cover ">
+    <div className="bg-[url('https://t4.ftcdn.net/jpg/03/50/81/89/360_F_350818931_54A6UVQiJIK8UHFWB0NTGSIKO9jyTbQP.jpg')] bg-cover  mb-16">
       <div className="py-40 bg-black/40 text-yellow-600">
         <div className="container mx-auto">
           <p>{subTitle}</p>
--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,12 +1,23 @@
 import SectionTitle from "../../Hooks/SectionTitle";
 
 const Contact = () => {
-    return (
-        <div>
-             <SectionTitle subTitle={"GYM Master"} title={"Contact"}></SectionTitle>
-            <h2>Contact</h2>
+  return (
+    <div>
+      <SectionTitle subTitle={"GYM Master"} title={"Contact"}></SectionTitle>
+      <div className="container mx-auto">
+        <div className="md:grid grid-cols-2">
+          <div className="md:w-1/2">
+            <h1 className="text-3xl font-bold">
+              We are here for help you! To Shape Your Body.
+            </h1>
+          </div>
+          <div className="md:w-1/2">
+            <h1>form</h1>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
