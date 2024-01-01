ace3cf7ca50c0f4fde18d1d783f2b8426500d997
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-01-01 23:37:09 +0600

    make contact sectionmmn

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -5,13 +5,39 @@ const Contact = () => {
     <div>
       <SectionTitle subTitle={"GYM Master"} title={"Contact"}></SectionTitle>
       <div className="container mx-auto">
-        <div className="md:grid grid-cols-2">
-          <div className="md:w-1/2">
-            <h1 className="text-3xl font-bold">
+        <div className="md:grid grid-cols-2 gap-10">
+          <div className="">
+            <h1 className="text-4xl font-bold">
               We are here for help you! To Shape Your Body.
             </h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque,
+              perferendis? Qui, magni temporibus commodi vitae consectetur
+              molestias? Dolore, exercitationem officia? Quidem tempore
+              temporibus optio, inventore nostrum molestias voluptatum
+              voluptates quae maxime repudiandae ipsum est quaerat, ullam
+              recusandae.
+            </p>
+            <div className="grid grid-cols-2">
+                <div>
+                    <h1 className="text-2xl font-bold">New York City, USA</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam aliquam placeat blanditiis dignissimos.</p>
+                </div>
+                <div>
+                    <h1 className="text-2xl font-bold">New York City, USA</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam aliquam placeat blanditiis dignissimos.</p>
+                </div>
+                <div>
+                    <h1 className="text-2xl font-bold">New York City, USA</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam aliquam placeat blanditiis dignissimos.</p>
+                </div>
+                <div>
+                    <h1 className="text-2xl font-bold">New York City, USA</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam aliquam placeat blanditiis dignissimos.</p>
+                </div>
+            </div>
           </div>
-          <div className="md:w-1/2">
+          <div className="">
             <h1>form</h1>
           </div>
         </div>
