eaae9dc54dd39226c755f650cf8a681fecd7e91e
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-01-02 22:50:10 +0600

    edit tym

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -20,20 +20,23 @@ const Contact = () => {
             </p>
             <div className="grid grid-cols-2 gap-10">
                 <div>
-                    <h1 className="text-2xl font-bold">New York City, USA</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam aliquam placeat blanditiis dignissimos.</p>
+                    <h1 className="text-2xl font-bold pb-3">New York City, USA</h1>
+                    <p>85 Briston Mint Street,</p>
+                    <p>London, E1 8LG, USA</p>
                 </div>
                 <div>
-                    <h1 className="text-2xl font-bold">New York City, USA</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam aliquam placeat blanditiis dignissimos.</p>
+                    <h1 className="text-2xl font-bold pb-3">Opening Hours</h1>
+                    <p>Mon to Fri: 7:30 am — 1:00 am</p>
+                    <p>Mon to Fri: 7:30 am — 1:00 am</p>
                 </div>
                 <div>
-                    <h1 className="text-2xl font-bold">New York City, USA</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam aliquam placeat blanditiis dignissimos.</p>
+                    <h1 className="text-2xl font-bold pb-3">Information</h1>
+                    <p>+800-123-4567</p>
+                    <p>gymaster@yourname.com</p>
                 </div>
                 <div>
-                    <h1 className="text-2xl font-bold">New York City, USA</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam aliquam placeat blanditiis dignissimos.</p>
+                    <h1 className="text-2xl font-bold pb-3">Follow Us On</h1>
+                    <p> Facebook,  instagram,  X,  linkedin</p>
                 </div>
             </div>
           </div>
