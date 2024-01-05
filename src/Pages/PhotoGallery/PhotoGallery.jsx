5f87faafbb4177d4e6b37b762c52baf3ffaa020f
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-01-05 22:45:00 +0600

    rmove lorem

--- src/Pages/PhotoGallery/PhotoGallery.jsx
+++ src/Pages/PhotoGallery/PhotoGallery.jsx
@@ -4,9 +4,7 @@ const PhotoGallery = () => {
     return (
         <div>
            <SectionTitle subTitle={"GYM Master"} title={"Galleries"}></SectionTitle> 
-            <div>
-                <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti modi libero non. Accusantium accusamus incidunt at, dolores a nihil fugit velit mollitia exercitationem quae, ab esse voluptate voluptatum, et quaerat?</h1>
-            </div>
+            
         </div>
     );
 };
