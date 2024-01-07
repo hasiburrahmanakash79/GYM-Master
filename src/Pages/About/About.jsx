b26aa73e93bd8b5bf465c57c567ab17c30557340
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-01-07 15:01:09 +0600

    make about

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -5,6 +5,7 @@ const About = () => {
         <div>
             <SectionTitle subTitle={"GYM Master"} title={"About"}></SectionTitle>
             <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis amet quaerat sunt, molestiae quas, itaque repellendus dicta minima aperiam consequatur quisquam eaque earum sed ipsa fugit doloremque natus alias nostrum!</p>
         </div>
     );
 };
