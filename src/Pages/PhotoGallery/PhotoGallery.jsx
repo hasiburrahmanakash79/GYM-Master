855400c60ee8cc83ec40afaf1811656eafabe85c
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-01-07 15:04:23 +0600

    make gallery

--- src/Pages/PhotoGallery/PhotoGallery.jsx
+++ src/Pages/PhotoGallery/PhotoGallery.jsx
@@ -4,7 +4,7 @@ const PhotoGallery = () => {
     return (
         <div>
            <SectionTitle subTitle={"GYM Master"} title={"Galleries"}></SectionTitle> 
-            
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit consequuntur dicta, deleniti veniam quae, impedit porro, modi eveniet repellat dignissimos sit quos maiores ullam molestias fugit vero. Reprehenderit, facere beatae.</p>
         </div>
     );
 };
