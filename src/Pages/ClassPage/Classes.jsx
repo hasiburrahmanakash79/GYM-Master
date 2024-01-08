e914f299f7f4c64f6d777ab65a9a51d073caa35f
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-01-08 15:57:18 +0600

    make makebgdf'

--- src/Pages/ClassPage/Classes.jsx
+++ src/Pages/ClassPage/Classes.jsx
@@ -6,7 +6,7 @@ const Classes = () => {
       <SectionTitle subTitle={"GYM Master"} title={"Classes"}></SectionTitle>
 
       <h1>Classes</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, at quas, deleniti tempore nisi, minus eligendi eum tenetur obcaecati officia unde quibusdam vel ab perspiciatis ipsa pariatur repellendus harum illo ea provident magni! Reprehenderit aliquam consequuntur esse odio dicta repellendus id in eius iure quia? Reprehenderit, optio. Sequi, earum minima?</p>
+      
     </div>
   );
 };
