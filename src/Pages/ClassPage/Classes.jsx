eb680afc31dacfae26bf44ad08009b13d0e5ef29
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-01-09 23:45:02 +0600

    chnge something

--- src/Pages/ClassPage/Classes.jsx
+++ src/Pages/ClassPage/Classes.jsx
@@ -6,7 +6,7 @@ const Classes = () => {
       <SectionTitle subTitle={"GYM Master"} title={"Classes"}></SectionTitle>
 
       <h1>Classes</h1>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas harum nostrum, neque cupiditate reprehenderit architecto ullam quas quisquam magnam expedita excepturi eligendi placeat pariatur maiores eveniet atque voluptate sequi quibusdam debitis dolorum ut tempora molestiae dolor. Enim molestiae nemo aut doloribus magnam sint assumenda hic dolorem, aliquid ab? Incidunt soluta beatae magnam? Illum blanditiis fugiat soluta consequuntur quasi vitae. Aut fugiat laborum, ea asperiores totam, blanditiis iste eligendi amet qui nihil maxime molestiae corporis velit at illum. Laudantium beatae ut laborum at ipsa architecto blanditiis consectetur dolorem ullam dolore eveniet totam alias, dolores sit enim repudiandae possimus, nam et iusto.</p>
+      
     </div>
   );
 };
