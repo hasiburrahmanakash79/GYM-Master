34c512912d9aa721eb8262ed8d8e3de18de6f049
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-01-09 23:46:19 +0600

    chnge something new

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -6,7 +6,7 @@ const Blog = () => {
         <div>
            <SectionTitle subTitle={"GYM Master"} title={"Blogs"}></SectionTitle>
           <h1>Blog</h1> 
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad et asperiores ullam eius a at quidem optio dolores, voluptate amet repellendus mollitia, animi, quam sed delectus ducimus quos? Illo, similique.</p> 
+          
         </div>
     );
 };
