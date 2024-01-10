c0c0c396b90ff1bb0eeddded8598179c1150493e
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-01-10 10:59:08 +0600

    try to make about

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -4,8 +4,9 @@ const About = () => {
     return (
         <div>
             <SectionTitle subTitle={"GYM Master"} title={"About"}></SectionTitle>
-            <h1>About</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis amet quaerat sunt, molestiae quas, itaque repellendus dicta minima aperiam consequatur quisquam eaque earum sed ipsa fugit doloremque natus alias nostrum!</p>
+            <div className="container mx-auto">
+
+            </div>
         </div>
     );
 };
