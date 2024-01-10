d56895023a69050be0f8fe3efb72a01cad6665e2
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-01-10 23:27:09 +0600

    set about image and text

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -5,8 +5,16 @@ const About = () => {
     <div>
       <SectionTitle subTitle={"GYM Master"} title={"About"}></SectionTitle>
       <div className="container mx-auto p-5">
-        <div className="grid grid-cols-2">
+        <div className="grid grid-cols-2 items-center gap-20">
+          <div>
             <img src="https://i.ibb.co/mHzkwPZ/about-1.jpg" alt="" />
+          </div>
+          <div className="space-y-7">
+            <p>Lorem, ipsum.</p>
+            <h1 className="text-5xl font-bold">Lorem ipsum dolor sit amet.</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas nihil qui sequi laboriosam cumque! Porro cupiditate esse quo ratione asperiores. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, harum voluptate expedita aliquam animi dicta ducimus? Adipisci, quaerat accusantium. Maxime?</p>
+            <button className="btn btn-primary">contact us</button>
+          </div>
         </div>
       </div>
     </div>
