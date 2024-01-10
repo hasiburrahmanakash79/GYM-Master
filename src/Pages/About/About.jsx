7761931ba35ac62a9996fbb4c930f38aba0e5603
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-01-10 23:22:55 +0600

    set about image

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -4,9 +4,9 @@ const About = () => {
   return (
     <div>
       <SectionTitle subTitle={"GYM Master"} title={"About"}></SectionTitle>
-      <div className="container mx-auto">
-        <div>
-            
+      <div className="container mx-auto p-5">
+        <div className="grid grid-cols-2">
+            <img src="https://i.ibb.co/mHzkwPZ/about-1.jpg" alt="" />
         </div>
       </div>
     </div>
