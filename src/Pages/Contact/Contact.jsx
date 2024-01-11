317f91cf23b5cabfadb7b5ff8f97e99511a47cce
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-01-11 22:54:24 +0600

    set input

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -19,29 +19,36 @@ const Contact = () => {
               recusandae.
             </p>
             <div className="grid grid-cols-2 gap-10">
-                <div>
-                    <h1 className="text-2xl font-bold pb-3">New York City, USA</h1>
-                    <p>85 Briston Mint Street,</p>
-                    <p>London, E1 8LG, USA</p>
-                </div>
-                <div>
-                    <h1 className="text-2xl font-bold pb-3">Opening Hours</h1>
-                    <p>Mon to Fri: 7:30 am — 1:00 am</p>
-                    <p>Mon to Fri: 7:30 am — 1:00 am</p>
-                </div>
-                <div>
-                    <h1 className="text-2xl font-bold pb-3">Information</h1>
-                    <p>+800-123-4567</p>
-                    <p>gymaster@yourname.com</p>
-                </div>
-                <div>
-                    <h1 className="text-2xl font-bold pb-3">Follow Us On</h1>
-                    <p> Facebook,  instagram,  X,  linkedin</p>
-                </div>
+              <div>
+                <h1 className="text-2xl font-bold pb-3">New York City, USA</h1>
+                <p>85 Briston Mint Street,</p>
+                <p>London, E1 8LG, USA</p>
+              </div>
+              <div>
+                <h1 className="text-2xl font-bold pb-3">Opening Hours</h1>
+                <p>Mon to Fri: 7:30 am — 1:00 am</p>
+                <p>Mon to Fri: 7:30 am — 1:00 am</p>
+              </div>
+              <div>
+                <h1 className="text-2xl font-bold pb-3">Information</h1>
+                <p>+800-123-4567</p>
+                <p>gymaster@yourname.com</p>
+              </div>
+              <div>
+                <h1 className="text-2xl font-bold pb-3">Follow Us On</h1>
+                <p> Facebook, instagram, X, linkedin</p>
+              </div>
             </div>
           </div>
-          <div className="">
-            <h1>form</h1>
+          <div className="bg-slate-100 ">
+            <label className="form-control w-full max-w-xs">
+              <span className="label-text">What is your name?</span>
+              <input
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered w-full"
+              />
+            </label>
           </div>
         </div>
       </div>
