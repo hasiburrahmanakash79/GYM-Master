30b82a3c08cfe635e0bdb73a99dcd2aed021ad47
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-01-13 22:45:25 +0600

    set branch

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -69,12 +69,7 @@ const Contact = () => {
                     <option disabled selected>
                       Who shot first?
                     </option>
-                    <option>Body building</option>
-                    <option>Running</option>
-                    <option>Fitness</option>
-                    <option>Workout</option>
-                    <option>Meditation</option>
-                    <option>Cycling</option>
+                    
                   </select>
                 </label>
               </div>
