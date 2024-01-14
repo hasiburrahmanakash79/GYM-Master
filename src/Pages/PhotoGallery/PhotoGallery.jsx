456891fbbb218ae53b1c4b35be77c1b21de89ffb
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-01-14 12:56:47 +0600

    make photo galery page

--- src/Pages/PhotoGallery/PhotoGallery.jsx
+++ src/Pages/PhotoGallery/PhotoGallery.jsx
@@ -4,7 +4,7 @@ const PhotoGallery = () => {
     return (
         <div>
            <SectionTitle subTitle={"GYM Master"} title={"Galleries"}></SectionTitle> 
-            
+            <h1>Photo gallery page</h1>
         </div>
     );
 };
