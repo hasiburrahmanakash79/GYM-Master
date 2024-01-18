2ba90eb6f0c034d2fe9a9aa7f8f71b4999718481
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-01-18 14:48:32 +0600

    set branch

--- src/Pages/PhotoGallery/PhotoGallery.jsx
+++ src/Pages/PhotoGallery/PhotoGallery.jsx
@@ -5,7 +5,7 @@ const PhotoGallery = () => {
         <div>
            <SectionTitle subTitle={"GYM Master"} title={"Galleries"}></SectionTitle> 
             <h1>Photo gallery page</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati culpa similique rerum adipisci reiciendis doloribus nam in accusantium officiis nihil. Suscipit labore voluptatem impedit cum obcaecati dolorem laboriosam maiores enim? Sed sequi minima, repudiandae voluptate asperiores tempore temporibus neque. Nam vitae possimus vel quaerat voluptates. Eveniet veniam impedit hic quas maiores. Harum aperiam a quasi eius mollitia exercitationem possimus minus enim quis atque ab earum officiis, hic quam accusantium tempore! Expedita, quibusdam! Dicta quia saepe inventore tempore laborum ipsum odio nemo, rerum distinctio pariatur voluptatibus quam obcaecati autem adipisci voluptatum corrupti laboriosam ex possimus dolorem voluptate labore officia, velit voluptas? Harum molestias sint quia iure, illum sit aperiam voluptatum optio laborum voluptatibus quos tempore rerum dolorum quae doloribus ad minima?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati culpa similique rerum adipisci reiciendis doloribus nam in accusantium officiis nihil. Suscipit labore voluptatem impedit cum obcaecati dolorem laboriosam maiores enim? Sed sequi minima, repudiandae voluptate asperiores tempore temporibus neque. Nam vitae possimus vel quaerat voluptates. Eveniet veniam impedit hic quas maiores. Harum aperiam a quasi eius mollitia exercitationem possimus minus enim quis atque ab earum officiis, hic quam accusantium tempore </p>
         </div>
     );
 };
