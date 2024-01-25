58ffbcb1eeaaf741cbf84655eb4816553adc2502
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-01-25 20:04:02 +0600

    make another blog

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -8,6 +8,7 @@ const Blog = () => {
           <h1>Blog</h1> 
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad et asperiores ullam eius a at quidem optio dolores, voluptate amet repellendus mollitia, animi, quam sed delectus ducimus quos? Illo, similique.</p> 
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi nostrum pariatur repudiandae totam ex cum quisquam rerum asperiores ipsa obcaecati?</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi nostrum pariatur repudiandae totam ex cum quisquam rerum asperiores ipsa obcaecati?</p>
         </div>
     );
 };
