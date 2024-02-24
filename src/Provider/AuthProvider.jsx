fb944e6de9baf8df6bcb2f2c5a530966e50ccddd
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-02-24 20:26:21 +0600

    make email signup

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,12 +1,26 @@
 import { useState } from "react";
 import { createContext } from "react";
+import app from "../Firebase/firebase.config";
+import {
+    createUserWithEmailAndPassword,
+    getAuth,
+  } from "firebase/auth";
 
 const AuthContext = createContext();
+const auth = getAuth(app);
 
 const AuthProvider = ({children}) => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const signUpUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
   const AuthInfo = {
     user,
+    loading,
+    signUpUser,
   };
   return <AuthContext.Provider value={AuthInfo}>{children} </AuthContext.Provider>;
 };
