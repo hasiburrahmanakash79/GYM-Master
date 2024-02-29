5fb9db4ffe3dd1ed8800cba9f4b047f6dcad2799
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-02-29 23:09:02 +0600

    make acenten ui

--- src/Pages/RegistrationPage/SignUp/SignUp.jsx
+++ src/Pages/RegistrationPage/SignUp/SignUp.jsx
@@ -1,10 +1,10 @@
-
+// import { SparklesCore } from "@/components/ui/sparkles";
 
 const SignUp = () => {
     return (
-        <div>
-            <p>registration</p>
-        </div>
+        <div className="h-[40rem] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
+      
+    </div>
     );
 };
 
