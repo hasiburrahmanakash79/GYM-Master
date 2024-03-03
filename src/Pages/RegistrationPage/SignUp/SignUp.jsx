9379bd66f6971ada4a1b64b4f620f70840e3c6d2
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-03-03 22:55:32 +0600

    create registration page

--- src/Pages/RegistrationPage/SignUp/SignUp.jsx
+++ src/Pages/RegistrationPage/SignUp/SignUp.jsx
@@ -1,43 +1,148 @@
 // import { SparklesCore } from "@/components/ui/sparkles";
 
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+
 const SignUp = () => {
+  const [passShow, setPassShow] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="">
-      <div className=" w-full relative max-w-xs">
-        <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-[0.80] bg-red-500 rounded-full blur-3xl" />
-        <div className="relative shadow-xl bg-gray-900 border border-gray-800  px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
-          <div className="h-5 w-5 rounded-full border flex items-center justify-center mb-4 border-gray-500">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="h-2 w-2 text-gray-300"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M4.5 4.5l15 15m0 0V8.25m0 11.25H8.25"
+      <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+            <div className="md:grid grid-cols-2 gap-3">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  {...register("name", { required: true })}
+                  type="text"
+                  placeholder="Enter your name"
+                  className="input input-bordered"
+                />
+                {errors.name && <span>This field is required</span>}
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  {...register("email", { required: true })}
+                  type="email"
+                  placeholder="Enter your email"
+                  className="input input-bordered"
+                />
+                {errors.email && <span>This field is required</span>}
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  {...register("password", {
+                    required: "Password field is required",
+                    minLength: {
+                      value: 6,
+                      message: "Password must be at least 6 characters",
+                    },
+                    
+                  })}
+                  type={passShow ? "text" : "password"}
+                  placeholder="Enter your password"
+                  className="input input-bordered"
+                />
+                <label className="label">
+                  <a className="label-text-alt link link-hover">
+                    <p onClick={() => setPassShow(!passShow)}>
+                      <small>
+                        {passShow ? (
+                          <span>Hide Pass</span>
+                        ) : (
+                          <span>Show Pass</span>
+                        )}
+                      </small>
+                    </p>
+                  </a>
+                </label>
+                {errors.password && <span>{errors.password.message}</span>}
+              </div>
+
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Confirm Password</span>
+                </label>
+                <input
+                  {...register("confirm", {
+                    required: "Confirm password field is required",
+                    validate: (value) =>
+                      value === password || "Passwords do not match",
+                  })}
+                  type={passShow ? "text" : "password"}
+                  placeholder="Confirm password"
+                  className="input input-bordered"
+                />
+                {errors.confirm && <span>{errors.confirm.message}</span>}
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Contact Number</span>
+                </label>
+                <input
+                  {...register("number", { required: true })}
+                  type="number"
+                  placeholder="+088"
+                  className="input input-bordered"
+                />
+                {errors.number && <span>This field is required</span>}
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Photo URL</span>
+                </label>
+                <input
+                  {...register("photo", { required: true })}
+                  type="text"
+                  placeholder="Photo url"
+                  className="input input-bordered"
+                />
+              </div>
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Address</span>
+              </label>
+              <input
+                {...register("address", { required: true })}
+                type="text"
+                placeholder="Enter your Address"
+                className="input input-bordered"
               />
-            </svg>
+            </div>
+            <div className="form-control mt-1">
+              <button className="btn btn-warning">Sign Up</button>
+            </div>
+          </form>
+          <div className="px-5 text-center">
+            <div className="divider mt-0 "></div>
+            <p className="font-semibold">Or Sign In with</p>
           </div>
-
-          <h1 className="font-bold text-xl text-white mb-4 relative z-50">
-            Meteors because they&apos;re cool
-          </h1>
-
-          <p className="font-normal text-base text-slate-500 mb-4 relative z-50">
-            I don&apos;t know what to write so I&apos;ll just paste something
-            cool here. One more sentence because lorem ipsum is just
-            unacceptable. Won&apos;t ChatGPT the shit out of this.
-          </p>
-
-          <button className="border px-4 py-1 rounded-lg  border-gray-500 text-gray-300">
-            Explore
-          </button>
-        </div>
-      </div>
+          <div className="text-center mb-7">
+            <Link to="/login">
+              Have an account?{" "}
+              <span className="text-yellow-700 font-bold">LogIn</span>{" "}
+            </Link>
+          </div>
     </div>
   );
 };
