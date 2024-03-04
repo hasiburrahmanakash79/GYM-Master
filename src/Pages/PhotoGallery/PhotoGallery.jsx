1e49ebe0bfc31935a2ea66756ccb26e1cf8b55a8
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2024-03-04 13:55:31 +0600

    start a photo gallery

--- src/Pages/PhotoGallery/PhotoGallery.jsx
+++ src/Pages/PhotoGallery/PhotoGallery.jsx
@@ -1,13 +1,59 @@
 import SectionTitle from "../../Hooks/SectionTitle";
 
 const PhotoGallery = () => {
-    return (
-        <div>
-           <SectionTitle subTitle={"GYM Master"} title={"Galleries"}></SectionTitle> 
-            <h1>Photo gallery page</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati culpa similique rerum adipisci reiciendis doloribus nam in accusantium officiis nihil. Suscipit labore voluptatem impedit cum obcaecati dolorem laboriosam maiores enim? Sed sequi minima, repudiandae voluptate asperiores tempore temporibus neque. Nam vitae possimus vel quaerat voluptates. Eveniet veniam impedit hic quas maiores. Harum aperiam a quasi eius mollitia exercitationem possimus minus enim quis atque ab earum officiis, hic quam accusantium tempore </p>
+  return (
+    <div>
+      <SectionTitle subTitle={"GYM Master"} title={"Galleries"}></SectionTitle>
+      <h1>Photo gallery page</h1>
+
+      <div>
+        <div className="carousel rounded-box">
+          <div className="carousel-item">
+            <img
+              src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg"
+              alt="Burger"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg"
+              alt="Burger"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg"
+              alt="Burger"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg"
+              alt="Burger"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg"
+              alt="Burger"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg"
+              alt="Burger"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg"
+              alt="Burger"
+            />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default PhotoGallery;
